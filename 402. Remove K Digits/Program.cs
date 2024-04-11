536fc81fc2488be2f2c5696a43dc3203f586f99c
Chris Bouwhuis <chrisb190203@gmail.com>
2024-04-11 12:05:45 +0200

    Add project files.

---
+++ 402. Remove K Digits/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassLib;
+namespace _402._Remove_K_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            string test = "1432219";
+            int k = 3;
+            string result = solution.RemoveKdigits(test, k);
+            Console.WriteLine(result);
+        }
+    }
+}
